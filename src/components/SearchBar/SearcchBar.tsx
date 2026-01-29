6a6d8d4aef0abe2789bf0ad5a93527fd07123b88
LisTRK <listrk54@gmail.com>
2026-01-29 14:33:24 +0200

    added correct search form

--- src/components/SearchBar/SearcchBar.tsx
+++ src/components/SearchBar/SearcchBar.tsx
@@ -1,34 +1,39 @@
 import toast from "react-hot-toast";
-import styles from "./SearchBar.module.css";
+import css from "./SearchBar.module.css";
+import { useState } from "react";
 
 interface SearchProps {
     onSubmit: (searchValue: string) => void;
 }
 
 const SearchBar = ({onSubmit}: SearchProps) => {
+    const [searchValue, setSearchValue] = useState("");
 
-    const handleSubmit = (formData: FormData) => {
-        const searchValue = formData.get("query") as string;
-        if (!searchValue.trim()) {
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        const trimmedValue = searchValue.trim()
+        if (!trimmedValue.trim()) {
             toast.error("Please enter your search query.");
             return 
         }
-        onSubmit(searchValue);
+        onSubmit(trimmedValue);
     }
 
-    return <header className={styles.header}>
-        <div className={styles.container}>
+    return <header className={css.header}>
+        <div className={css.container}>
             
-            <form action={handleSubmit} className={styles.form}>
+            <form onSubmit={handleSubmit} className={css.form}>
                 <input
-                    className={styles.input}
+                    className={css.input}
                     type="text"
                     name="query"
                     autoComplete="off"
                     placeholder="Search products..."
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
                     autoFocus
                 />
-                <button className={styles.button} type="submit">
+                <button className={css.button} type="submit">
                     Search
                 </button>
             </form>
